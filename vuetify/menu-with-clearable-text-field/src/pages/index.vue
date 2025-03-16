49fc70e6273e24e2bfb2bb7afc654a7505dc5762
Jaroslav Brtis <6890442+Jardo-51@users.noreply.github.com>
2025-03-16 12:43:34 +0100

    Text field made clearable (introduces Vuetify error).

--- vuetify/menu-with-clearable-text-field/src/pages/index.vue
+++ vuetify/menu-with-clearable-text-field/src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-menu open-on-focus>
     <template v-slot:activator="{ props }">
-      <v-text-field v-bind="props" />
+      <v-text-field v-bind="props" clearable />
     </template>
     <p>content</p>
   </v-menu>
