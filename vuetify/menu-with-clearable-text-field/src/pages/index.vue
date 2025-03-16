325c0cdbc8475898bc18f6728b700ef56a2bb4e2
Jaroslav Brtis <6890442+Jardo-51@users.noreply.github.com>
2025-03-16 12:39:27 +0100

    Added Vuetify issue MRE.

---
+++ vuetify/menu-with-clearable-text-field/src/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-menu open-on-focus>
+    <template v-slot:activator="{ props }">
+      <v-text-field v-bind="props" />
+    </template>
+    <p>content</p>
+  </v-menu>
+</template>
+
+<script setup>
+  //
+</script>
